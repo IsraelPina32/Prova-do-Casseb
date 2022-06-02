3584c70b8b9f806fced7c7fed5a8ab37419acdb8
israell <israelpina047@gmail.com>
2022-06-02 15:29:06 -0300

    falta so o js

---
+++ index.js
@@ -0,0 +1,15 @@
+function click() {
+    var ano = currentTime.getfullyears();
+    var mes = currentTime.getUTCMonth();
+    var dia = currentTime.getDate();
+    for (let i = 0; i < dia.length; i++) {
+    
+    }
+    for (let i = 0; i < mes.length; i++) {
+          
+    }
+    for (let i = 0; i < ano.length; i++) {
+        
+    }
+    
+}
